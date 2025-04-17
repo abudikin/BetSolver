0b7743dbf87dec652a6b09d8b4aa295ee6f9f30a
abudikin <54790361+abudikin@users.noreply.github.com>
2025-04-17 20:58:49 +0900

    q

--- src/backend/src/main.ts
+++ src/backend/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3020);
+  await app.listen(3030);
 }
 bootstrap();
