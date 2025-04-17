7b918ec9483b93c7d5342b3774283c2e9ebafe93
abudikin <54790361+abudikin@users.noreply.github.com>
2025-04-17 20:51:28 +0900

    q

--- src/backend/src/auth/strategies/jwt.strategy.ts
+++ src/backend/src/auth/strategies/jwt.strategy.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { User } from '../entities/user.entity';
+import { User } from 'entities/user.entity';
 import { UsersService } from '../../users/users.service';
 
 @Injectable()
